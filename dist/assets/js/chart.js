0a4e21e426dade83ffc1f083dd24ec98e76f6521
SEWAH <2ksewah@gmail.com>
2024-03-23 05:59:51 +0000

    refactor: Rajout de charts sur la page d'accueil

--- dist/assets/js/chart.js
+++ dist/assets/js/chart.js
@@ -1,92 +1,175 @@
-// Partie Statistique
+// Partie Chart vente totale casier
 
-// var options = {
-//     series: [{
-//     name: 'Nombre de ventes ',
-//     data: [20, 10, 15, 8, 7, 20, 6, 25, 30, 18, 15, 100]
-//   }],
-//     chart: {
-//     height: 350,
-//     type: 'bar',
-//   },
-//   plotOptions: {
-//     bar: {
-//       borderRadius: 10,
-//       dataLabels: {
-//         position: 'top', // top, center, bottom
-//       },
-//     }
-//   },
-//   dataLabels: {
-//     enabled: true,
-//     formatter: function (val) {
-//       return val + "";
-//     },
-//     offsetY: -20,
-//     style: {
-//       fontSize: '15px',
-//       colors: ["#304758"]
-//     }
-//   },
+var options = {
+    series: [{
+    name: 'Nombre de ventes ',
+    data: [400, 200, 300, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  }],
+    chart: {
+    height: 350,
+    type: 'bar',
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 10,
+      dataLabels: {
+        position: 'center', // top, center, bottom
+      },
+    }
+  },
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+      return val + "";
+    },
+    offsetY: -20,
+    style: {
+      fontSize: '15px',
+      colors: ["#fff"]
+    }
+  },
   
-//   xaxis: {
-//     categories: ["Jan", "Fev", "Mar", "Avr", "Mai", "Juin", "Juil", "Aôut", "Sept", "Oct", "Nov", "Dec"],
-//     position: 'top',
-//     axisBorder: {
-//       show: false
-//     },
-//     axisTicks: {
-//       show: false
-//     },
-//     crosshairs: {
-//       fill: {
-//         type: 'gradient',
-//         gradient: {
-//           colorFrom: '#D8E3F0',
-//           colorTo: '#BED1E6',
-//           stops: [0, 100],
-//           opacityFrom: 0.4,
-//           opacityTo: 0.5,
-//         }
-//       }
-//     },
-//     tooltip: {
-//       enabled: true,
-//     }
-//   },
-//   yaxis: {
-//     axisBorder: {
-//       show: false
-//     },
-//     axisTicks: {
-//       show: false,
-//     },
-//     labels: {
-//       show: false,
-//       formatter: function (val) {
-//         return val + "";
-//       }
-//     }
+  xaxis: {
+    categories: ["Jan", "Fev", "Mar", "Avr", "Mai", "Juin", "Juil", "Aôut", "Sept", "Oct", "Nov", "Dec"],
+    position: 'top',
+    axisBorder: {
+      show: false
+    },
+    axisTicks: {
+      show: false
+    },
+    crosshairs: {
+      fill: {
+        type: 'gradient',
+        gradient: {
+          colorFrom: '#D8E3F0',
+          colorTo: '#BED1E6',
+          stops: [0, 100],
+          opacityFrom: 0.4,
+          opacityTo: 0.5,
+        }
+      }
+    },
+    tooltip: {
+      enabled: true,
+    }
+  },
+  yaxis: {
+    axisBorder: {
+      show: false
+    },
+    axisTicks: {
+      show: false,
+    },
+    labels: {
+      show: false,
+      formatter: function (val) {
+        return val + "";
+      }
+    },
+    max: 500, // Définit la valeur maximale de l'axe Y pour la deuxième série de données (Prévision)
+    min: 0, // Définit la valeur minimale de l'axe Y pour la deuxième série de données (Prévision)
+    tickAmount: 1 
+
   
-//   },
-//   title: {
-//     text: 'Statistique des ventes à ETS GLORY-WIJOSPI, 2023',
-//     floating: true,
-//     offsetY: 330,
-//     align: 'center',
-//     style: {
-//       color: '#444'
-//     }
-//   }
-//   };
+  },
+  title: {
+    text: 'Nombre total de vente par mois / 2024',
+    floating: true,
+    offsetY: 330,
+    align: 'center',
+    style: {
+      color: '#444'
+    }
+  }
+  };
 
-//   var chart = new ApexCharts(document.querySelector(".chart"), options);
-//   chart.render();
+  var chart = new ApexCharts(document.querySelector(".chartHome"), options);
+  chart.render();
 
-//Fin Partie Statistique
+//Fin Chart vente Totale casier
 
+//Chart pourcentage Homme / Femme 
 
-//Partie Accueil
+var options = {
+  series: [190, 74],
+  chart: {
+  width: '100%',
+  type: 'pie',
+},
+labels: ["Homme", "Femme"],
+theme: {
+  monochrome: {
+    enabled: false
+  }
+},
+plotOptions: {
+  pie: {
+    dataLabels: {
+      offset: -5
+    }
+  }
+},
+title: {
+  text: "Client par sexe"
+},
+dataLabels: {
+  formatter(val, opts) {
+    const name = opts.w.globals.labels[opts.seriesIndex]
+    return [name, val.toFixed(1) + '%']
+  }
+},
+legend: {
+  show: true
+}
+};
+
+var chart = new ApexCharts(document.querySelector(".chartPourcentageSexe"), options);
+chart.render();
+
+//Fin Chart pourcentage Homme / Femme
+
+//Chart nombre de vente par produit
+
+var options = {
+  series: [5, 10, 50, 20, 15],
+  chart: {
+  width: '100%',
+  type: 'pie',
+},
+labels: ["Coca-cola", "Cocktail de fruits", "Pils", "Lager", "Racine"],
+theme: {
+  monochrome: {
+    enabled: false
+  }
+},
+plotOptions: {
+  pie: {
+    dataLabels: {
+      offset: -5
+    }
+  }
+},
+title: {
+  text: "Nombre de casiers vendus par produit"
+},
+dataLabels: {
+  formatter(val, opts) {
+    const name = opts.w.globals.labels[opts.seriesIndex]
+    return [name, val.toFixed(1) + '%']
+  }
+},
+legend: {
+  show: true
+}
+};
+
+var chart = new ApexCharts(document.querySelector(".chartPourcentageVente"), options);
+chart.render();
+
+//Fin Chart nombre de vente par produit
+
+//Partie Statistique
 
 var options = {
     series: [
@@ -238,4 +321,4 @@ yaxis: {
   var chart = new ApexCharts(document.querySelector(".chart"), options);
   chart.render();
 
-//Fin Partie Accueil
+//Fin Partie Statistique
